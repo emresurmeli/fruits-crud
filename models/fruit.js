15798a6827ef38f5101e97e25327c994d47364d3
emresurmeli <s.emre.s.8@gmail.com>
2025-03-24 15:09:37 -0400

    Add strater code

---
+++ models/fruit.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const fruitSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  isReadyToEat: Boolean,
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+export default Fruit;
