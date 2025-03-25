baa22f97bc3f48a88434ca4302cd6e9d3b9f522d
emresurmeli <s.emre.s.8@gmail.com>
2025-03-25 13:31:37 -0400

    Add PUT and DELETE routes and templates

--- controllers/fruits.js
+++ controllers/fruits.js
@@ -12,6 +12,14 @@ fruitsRouter.get("/fruits/new", (req, res) => {
   res.render("fruits/new");
 });
 
+// Get the edit form
+fruitsRouter.get("/fruits/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const fruit = await Fruit.findById(id);
+
+  res.render("fruits/edit", { fruit });
+});
+
 fruitsRouter.get("/fruits/:id", async (req, res) => {
   const { id } = req.params;
   const fruit = await Fruit.findById(id);
@@ -36,6 +44,35 @@ fruitsRouter.post("/fruits", async (req, res) => {
   }
 
   const fruit = await Fruit.create({ name, isReadyToEat });
+
+  res.redirect("/fruits");
+});
+
+fruitsRouter.put("/fruits/:id", async (req, res) => {
+  // Grab the id from the params
+  const { id } = req.params;
+  const updateData = {
+    name: req.body.name,
+    isReadyToEat: req.body.isReadyToEat,
+  };
+
+  if (updateData.isReadyToEat) {
+    updateData.isReadyToEat = true;
+  } else {
+    updateData.isReadyToEat = false;
+  }
+
+  // Get the fruit and update it
+  await Fruit.findByIdAndUpdate(id, updateData, {
+    returnDocument: "after",
+  });
+  // redirect to fruit
+  res.redirect(`/fruits/${id}`);
+});
+
+fruitsRouter.delete("/fruits/:id", async (req, res) => {
+  const { id } = req.params;
+  await Fruit.findByIdAndDelete(id);
 
   res.redirect("/fruits");
 });
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import morgan from "morgan";
+import methodOverride from "method-override";
 import "./db/connection.js";
 
 import fruitsRouter from "./controllers/fruits.js";
@@ -8,7 +10,8 @@ const app = express();
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 app.set("view engine", "ejs");
 
 // Routes
